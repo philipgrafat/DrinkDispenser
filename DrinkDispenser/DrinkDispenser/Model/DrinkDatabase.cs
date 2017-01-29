4173b380e6e81fd802f43647719d6f83a1c7c299
Philip Graf <Philip Graf@Home>
2017-01-29 19:24:27 +0100

    Added the DrinkDatabase class and added ProtoBuf-NET as the serializer

---
+++ DrinkDispenser/DrinkDispenser/Model/DrinkDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DrinkDispenser.Model
+{
+    public class DrinkDatabase
+    {
+        private List<Drink> _drinks;
+
+        public void Load()
+        {
+            
+        }
+    }
+}
